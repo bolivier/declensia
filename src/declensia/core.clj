17cc64f1b673d90cb63262ce1cb3d4e6c3a4b6d9
Brandon Olivier <brandon@brandonolivier.com>
2025-06-25 18:41:07 -0500

    Add big test suite

--- src/declensia/core.clj
+++ src/declensia/core.clj
@@ -35,17 +35,139 @@
                                       :rule   rule})))
                    (fn [rules] (update rules rule-type #(cons rule %))))))
 
+(add-rule :plural :irregular (rule ::octo "ox" "oxen"))
+(add-rule :singular :irregular (rule ::octo "oxen" "ox"))
 (add-rule :plural :irregular (rule ::octo "octopus" "octopi"))
+(add-rule :plural :irregular (rule ::person "person" "people"))
+(add-rule :singular :irregular (rule ::person "people" "person"))
+(add-rule :plural :irregular (rule ::children "child" "children"))
+(add-rule :singular :irregular (rule ::children "children" "child"))
+(add-rule :singular :irregular (rule ::mice #"(?i)(l|m)ice" "$1ouse"))
+(add-rule :plural :irregular (rule ::mice #"(?i)(l|m)ouse" "$1ice"))
+
+(doseq [w ["air"
+           "alcohol"
+           "art"
+           "blood"
+           "butter"
+           "cheese"
+           "chewing"
+           "coffee"
+           "confusion"
+           "cotton"
+           "education"
+           "electricity"
+           "entertainment"
+           "equipment"
+           "experience"
+           "fiction"
+           "fish"
+           "food"
+           "forgiveness"
+           "fresh"
+           "gold"
+           "gossip"
+           "grass"
+           "ground"
+           "gum"
+           "happiness"
+           "history"
+           "homework"
+           "honey"
+           "ice"
+           "information"
+           "jam"
+           "jeans"
+           "knowledge"
+           "lightning"
+           "liquid"
+           "literature"
+           "love"
+           "luck"
+           "luggage"
+           "meat"
+           "milk"
+           "mist"
+           "money"
+           "music"
+           "news"
+           "oil"
+           "oxygen"
+           "paper"
+           "patience"
+           "peanut"
+           "pepper"
+           "petrol"
+           "police"
+           "pork"
+           "power"
+           "pressure"
+           "research"
+           "rice"
+           "sadness"
+           "series"
+           "sheep"
+           "shopping"
+           "silver"
+           "snow"
+           "space"
+           "species"
+           "speed"
+           "steam"
+           "sugar"
+           "sunshine"
+           "tea"
+           "tennis"
+           "thunder"
+           "time"
+           "toothpaste"
+           "traffic"
+           "up"
+           "vinegar"
+           "washing"
+           "weather"
+           "wine"
+           "wood"
+           "wool"]]
+  (add-rule :singular :uncountable (rule (keyword w) w w))
+  (add-rule :plural :uncountable (rule (keyword w) w w)))
 
 (doseq [r [(rule ::base #"$" "s")
-           (rule ::xes #"(?i)(ax|test)is" "$1es")
-           (rule ::us #"(?i)(.*)us" "$1uses")
-           (rule ::octo #"(?i)(octop)us" "$1i")]]
+           (rule ::paren #"(?i)(.*)sis$" "$1ses")
+           (rule ::man #"(wom|m)an$" "$1en")
+           (rule ::life #"(.*)fe?$" "$1ves")
+           (rule ::xes #"(?i)(ax|test)is$" "$1es")
+           (rule ::es #"(?i)(x|ch|ss|sh)$" "$1es")
+           (rule ::ices #"(?i)(matr|vert|ind)(?:ix|ex)$" "$1ices")
+           (rule ::buffalo #"(?i)(buffal|tomat)o$" "$1oes")
+           (rule ::as #"(?i)(.*)as$" "$1ases")
+           (rule ::media #"(?i)([ti])um$" "$1a")
+           (rule ::query #"(?i)(quer)y$" "$1ies")
+           (rule ::us #"(?i)(.*)us$" "$1uses")
+           (rule ::octo #"(?i)(octop)us$" "$1i")
+           (rule ::crisis #"(?i)(cris|test)(is|es)$" "$1es")
+           (rule ::quiz #"(?i)(qui)z$" "$1zzes")]]
   (add-rule :plural r))
 
 (doseq [r [(rule ::base #"s$" "")
+           (rule ::life #"(?i)([^f])ves$" "$1fe")
+           (rule ::hive #"(?i)(hive)s$" "$1")
+           (rule ::persp #"(?i)(tive)s$" "$1")
+           (rule ::thief #"thieves" "thief")
+           (rule ::vertex #"(?i)(vert|ind)ices$" "$1ex")
+           (rule ::matrix #"(?i)(matr)ices$" "$1ix")
+           (rule ::paren #"(?i)(.*)ses$" "$1sis")
+           (rule ::man #"(wom|m)en$" "$1an")
+           (rule ::media #"(?i)([ti])a$" "$1um")
+           (rule ::alias #"(?i)(alias)es$" "$1")
+           (rule ::buffalo #"(?i)(o)es$" "$1")
+           (rule ::series #"(?i)(quer)ies$" "$1y")
+           (rule ::shoes #"shoes" "shoe")
+           (rule :es #"(?i)(x|ch|ss|sh)es$" "$1")
            (rule ::xes #"^(a)x[ie]s$" "$1xis")
-           (rule ::octo #"(?i)(.*)(us|i|uses)$" "$1us")]]
+           (rule ::octo #"(?i)(.*)(us|i|uses)$" "$1us")
+           (rule ::crisis #"(?i)(cris|test)(is|es)$" "$1is")
+           (rule ::quiz #"(?i)(quiz)zes$" "$1")]]
   (add-rule :singular r))
 
 (defmulti pluralize
--- test/declensia/core_test.clj
+++ test/declensia/core_test.clj
@@ -11,16 +11,111 @@
    ["octopus" "octopi"]
    ["virus" "viruses"]
    ["bus" "buses"]
-   ["status" "statuses"]])
+   ["status" "statuses"]
+   ["man" "men"]
+   ["woman" "women"]
+   ["person" "people"]
+   ["sex" "sexes"]
+   ["child" "children"]
+   ["mouse" "mice"]
+   ["shoe" "shoes"]
+   ["crisis" "crises"]
+   ["alias" "aliases"]
+   ["zombie" "zombies"]
+   ["parenthesis" "parentheses"]
+   ["analysis" "analyses"]
+   "equipment"
+   "information"
+   "rice"
+   "money"
+   "species"
+   "series"
+   "fish"
+   "sheep"
+   "jeans"
+   "police"
+   ["ox" "oxen"]
+   ["life" "lives"]
+   ["thief" "thieves"]
+   ["quiz" "quizzes"]
+   ["ability" "abilities"]
+   [:ability :abilities]
+   ['ability 'abilities]
+   ["address" "addresses"]
+   ["address" "address"]
+   ["agency" "agencies"]
+   ["alias" "aliases"]
+   ["alias" "alias"]
+   ["amenity" "amenities"]
+   ["analysis" "analyses"]
+   ["analysis" "analysis"]
+   ["archive" "archives"]
+   ["axis" "axes"]
+   ["basis" "bases"]
+   ["box" "boxes"]
+   ["buffalo" "buffaloes"]
+   ["bus" "buses"]
+   ["bus" "bus"]
+   ["case" "cases"]
+   ["category" "categories"]
+   ["comment" "comments"]
+   ["crisis" "crises"]
+   ["crisis" "crisis"]
+   ["database" "databases"]
+   ["datum" "data"]
+   ["day" "days"]
+   ["diagnosis" "diagnoses"]
+   ["diagnosis" "diagnosis"]
+   ["dwarf" "dwarves"]
+   ["edge" "edges"]
+   ["elf" "elves"]
+   ["experience" "experiences"]
+   ["fix" "fixes"]
+   ["foobar" "foobars"]
+   ["half" "halves"]
+   ["horse" "horses"]
+   ["house" "houses"]
+   ["index" "indices"]
+   ["louse" "lice"]
+   ["matrix" "matrices"]
+   ["medium" "media"]
+   ["mouse" "mice"]
+   ["movie" "movies"]
+   ["newsletter" "newsletters"]
+   ["octopus" "octopi"]
+   ["ox" "oxen"]
+   ["perspective" "perspectives"]
+   ["photo" "photos"]
+   ["portfolio" "portfolios"]
+   ["prize" "prizes"]
+   ["process" "processes"]
+   ["query" "queries"]
+   ["quiz" "quizzes"]
+   ["safe" "saves"]
+   ["search" "searches"]
+   ["shoe" "shoes"]
+   ["stack" "stacks"]
+   ["status" "statuses"]
+   ["switch" "switches"]
+   ["testis" "testes"]
+   ["tomato" "tomatoes"]
+   ["vertex" "vertices"]
+   ["wife" "wives"]
+   ["wish" "wishes"]
+   ["weather" "weather"]])
+
 
 (e/defexpect plural-singular-test
-  (doseq [[singular plural] cases]
-    (e/expect plural
-              (sut/pluralize singular)
-              (str "Failed to pluralize " singular))
-    (e/expect singular
-              (sut/singularize plural)
-              (str "Failed to singularize " plural))))
+  (doseq [case cases]
+    (let [[singular plural] (if (vector? case)
+                              case
+                              [case case])]
+      (e/expect plural
+                (sut/pluralize singular)
+                (str "Failed to pluralize " singular))
+      (e/expect singular
+                (sut/singularize plural)
+                (str "Failed to singularize " plural)))))
 
-(let [word "axis"]
+(let [word "octopus"]
   [(sut/singularize (sut/pluralize word)) (sut/pluralize word)])
